8f7ba4fa346e9c8d5c799b0b8d197d3a3428dda8
samll-howe <2678691035@qq.com>
2020-01-02 14:57:44 +0800

    Hystrix 服务熔断和Listening监听

--- springcloud-consumer-hystrix-listening/src/main/java/com/tangwh/DeptConsumerListening.java
+++ springcloud-consumer-hystrix-listening/src/main/java/com/tangwh/DeptConsumerListening.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 
 @SpringBootApplication
 @EnableHystrixDashboard //开启监控页面
+
 public class DeptConsumerListening {
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumerListening.class, args);
